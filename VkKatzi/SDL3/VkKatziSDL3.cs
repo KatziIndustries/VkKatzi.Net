d989c0662fed9528c202e403be4f938607ffc120
Aaron_LW <aaronl.waechter@gmail.com>
2026-08-28 13:36:02 +0200

    Fix typo

--- VkKatzi/SDL3/VkKatziSDL3.cs
+++ VkKatzi/SDL3/VkKatziSDL3.cs
@@ -5,5 +5,5 @@ namespace VkKatzi.SDL3;
 public static class VKK_SDL
 {
     [DllImport("vkkatzi_SDL3", CallingConvention = CallingConvention.Cdecl, EntryPoint = "VKK_CreateSurfaceSDL")]
-    public static extern Result CreateSurface(IntPtr window, InstanceHandle instance, out SurfaceHandle surface);
+    public static extern VKK.Result CreateSurface(IntPtr window, InstanceHandle instance, out SurfaceHandle surface);
 }
